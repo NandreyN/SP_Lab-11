8bfefac06006be7753efbdd82cbcd6882fb850e0
Андрей <andrey.naumovich@mail.ru>
2018-05-06 20:09:54 +0300

    Display objects in table

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,20 +1,57 @@
 package controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 import javafx.scene.control.MenuBar;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import model.Client;
+import model.ClientCollectionParser;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
 
 public class Controller {
 
     @FXML
-    private ListView listView;
+    private TableView<Client> tableView;
+
+    @FXML
+    private TableColumn<Client, Integer> idColumn;
+    @FXML
+    private TableColumn<Client, Integer> ageColumn;
+    @FXML
+    private TableColumn<Client, String> nameColumn;
+    @FXML
+    private TableColumn<Client, String> eMailColumn;
+    @FXML
+    private TableColumn<Client, String> phoneColumn;
+    @FXML
+    private TableColumn<Client, String> statusColumn;
+
     @FXML
     private MenuBar menuBar;
 
-    public Controller() {
+
+    private ObservableList<Client> clientObservableList =
+            FXCollections.observableArrayList(new ClientCollectionParser().getCollection());
+
+    public Controller() throws IOException, SAXException, ParserConfigurationException {
     }
 
     @FXML
     protected void initialize() {
+        idColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
+        ageColumn.setCellValueFactory(new PropertyValueFactory<>("age"));
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        eMailColumn.setCellValueFactory(new PropertyValueFactory<>("email"));
+        phoneColumn.setCellValueFactory(new PropertyValueFactory<>("phone"));
+        statusColumn.setCellValueFactory(new PropertyValueFactory<>("status"));
+
+        tableView.setItems(clientObservableList);
     }
 }
--- src/model/Client.java
+++ src/model/Client.java
@@ -1,6 +1,54 @@
 package model;
 
 public class Client {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Email getEmail() {
+        return email;
+    }
+
+    public void setEmail(Email email) {
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
     public enum Status {
         PREMIUM, STANDARD
     }
@@ -11,7 +59,7 @@ public class Client {
     private int age;
     private Status status;
 
-    public Client(String name, String email,String phone, int id, int age, Status status) {
+    public Client(String name, String email, String phone, int id, int age, Status status) {
         this.name = name;
         this.phone = phone;
         this.email = new Email(email);
